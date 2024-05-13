69b72357b5271f546e7cb1cfad24b771f91a58ff
dialex24 <diegomerida24112003@gmail.com>
2024-05-13 14:41:53 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFecha = New System.Windows.Forms.TextBox()
+        Me.txtLugar = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHora = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.chkUrgente = New System.Windows.Forms.CheckBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MV Boli", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(297, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(189, 52)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "AGENDA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(268, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fecha"
+        '
+        'txtFecha
+        '
+        Me.txtFecha.Location = New System.Drawing.Point(341, 100)
+        Me.txtFecha.Name = "txtFecha"
+        Me.txtFecha.Size = New System.Drawing.Size(180, 22)
+        Me.txtFecha.TabIndex = 2
+        '
+        'txtLugar
+        '
+        Me.txtLugar.Location = New System.Drawing.Point(341, 137)
+        Me.txtLugar.Name = "txtLugar"
+        Me.txtLugar.Size = New System.Drawing.Size(180, 22)
+        Me.txtLugar.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(268, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Lugar"
+        '
+        'txtHora
+        '
+        Me.txtHora.Location = New System.Drawing.Point(341, 174)
+        Me.txtHora.Name = "txtHora"
+        Me.txtHora.Size = New System.Drawing.Size(180, 22)
+        Me.txtHora.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(268, 181)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Hora"
+        '
+        'chkUrgente
+        '
+        Me.chkUrgente.AutoSize = True
+        Me.chkUrgente.Location = New System.Drawing.Point(371, 213)
+        Me.chkUrgente.Name = "chkUrgente"
+        Me.chkUrgente.Size = New System.Drawing.Size(77, 20)
+        Me.chkUrgente.TabIndex = 7
+        Me.chkUrgente.Text = "Urgente"
+        Me.chkUrgente.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(104, 252)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(613, 167)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(608, 113)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(134, 79)
+        Me.btnAgregar.TabIndex = 9
+        Me.btnAgregar.Text = "Agregrar Cita"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.chkUrgente)
+        Me.Controls.Add(Me.txtHora)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtLugar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtFecha)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Agenda"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtFecha As TextBox
+    Friend WithEvents txtLugar As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtHora As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents chkUrgente As CheckBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnAgregar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    ' Crear una lista para almacenar las citas
+    Dim citas As New List(Of Cita)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configurar el DataGridView
+        DataGridView1.ColumnCount = 4
+        DataGridView1.Columns(0).Name = "Fecha"
+        DataGridView1.Columns(1).Name = "Lugar"
+        DataGridView1.Columns(2).Name = "Hora"
+        DataGridView1.Columns(3).Name = "Urgente"
+    End Sub
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        ' Obtener los datos de la nueva cita
+        Dim fecha As String = txtFecha.Text
+        Dim lugar As String = txtLugar.Text
+        Dim hora As String = txtHora.Text
+        Dim urgente As Boolean = chkUrgente.Checked
+
+        ' Crear una nueva instancia de Cita
+        Dim nuevaCita As New Cita(fecha, lugar, hora, urgente)
+
+        ' Agregar la cita a la lista
+        citas.Add(nuevaCita)
+
+        ' Mostrar las citas en el DataGridView
+        MostrarCitas()
+    End Sub
+
+    Private Sub MostrarCitas()
+        ' Limpiar el DataGridView
+        DataGridView1.Rows.Clear()
+
+        ' Ordenar la lista de citas (mostrar las urgentes primero)
+        citas.Sort(Function(x, y) y.Urgente.CompareTo(x.Urgente))
+
+        ' Mostrar las citas en el DataGridView
+        For Each cita In citas
+            Dim urgenteString As String
+            If cita.Urgente Then
+                urgenteString = "Sí"
+            Else
+                urgenteString = "No"
+            End If
+            DataGridView1.Rows.Add({cita.Fecha, cita.Lugar, cita.Hora, urgenteString})
+        Next
+    End Sub
+
+End Class
+
+Public Class Cita
+    Public Property Fecha As String
+    Public Property Lugar As String
+    Public Property Hora As String
+    Public Property Urgente As Boolean
+
+    Public Sub New(ByVal fecha As String, ByVal lugar As String, ByVal hora As String, ByVal urgente As Boolean)
+        Me.Fecha = fecha
+        Me.Lugar = lugar
+        Me.Hora = hora
+        Me.Urgente = urgente
+    End Sub
+End Class
+
